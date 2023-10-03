35a1d66258cf213c4ce440ad1fd53261ba2f18c3
Prateek Lodhi <mailprateeklodhi@gmail.com>
2023-10-03 12:30:58 +0530

    models

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 import json
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
-
-# Create your views here.
+from product.models import Product
 def api_home(request,*args,**kwargs):
-    body = request.body
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-        print(data)
-    except:
-        pass
-    data["headers"] = dict(request.headers)
-    data["content_type"] = request.content_type
-    print(body)
+    if model_data:
+        data = model_to_dict(model_data)
+        # data["id"] = model_data.id
+        # data['title'] = model_data.title
+        # data["content"] = model_data.content
+        # data["price"] = model_data.price
+
+    # body = request.body
+    # data = {}
+    # try:
+    #     data['body'] = json.loads(body)
+    # except:
+    #     pass
+    # data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
+    # print(f"{data['headers']}\n{data['content_type']}")
+
     return JsonResponse(data)
+    
